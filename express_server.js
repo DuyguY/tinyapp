4938bab869c0f5d674c84d2b399edde777fca787
DuyguY <duygu.yldrm4820@gmail.com>
2021-12-07 13:39:20 -0500

    Installed nodemon

--- express_server.js
+++ express_server.js
@@ -49,6 +49,11 @@ app.get("/set", (req, res) => {
   res.send(`a = ${a}`);
  });
 
+ app.get("/u/:shortURL", (req, res) => {
+   const longURL = urlDatabase[req.params.shortURL]
+  res.redirect(longURL);
+});
+
  app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
   res.send("Ok");         // Respond with 'Ok' (we will replace this)
