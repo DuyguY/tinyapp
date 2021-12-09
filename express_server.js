b139b56e7254c2b815f595504c5627c242878e14
DuyguY <duygu.yldrm4820@gmail.com>
2021-12-09 17:10:04 -0500

    added login feature

--- express_server.js
+++ express_server.js
@@ -7,6 +7,19 @@ const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
+const users = { 
+  "user1ID": {
+    id: "user1ID", 
+    email: "user@test.com", 
+    password: "pink-cat"
+  },
+ "user2ID": {
+    id: "user2ID", 
+    email: "user2@test.com", 
+    password: "brown-dog"
+  }
+}
+
 
 app.set("view engine", "ejs");
 
@@ -16,8 +29,31 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const findUserByEmail = (email) => {
+  for (const userId in users) {
+    const user = users[userId];
+    if(user.email === email) {
+      return user;
+    }
+  }
+  return null;
+}
+
+function generateRandomString() {
+  let result = '';
+  characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  for (let i = 0; i < 6; i++) {
+    result += characters[Math.floor(Math.random() * characters.length)];
+  }
+  return result;
+}
+
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  if (req.cookies.user_id) {
+    res.redirect('/urls');
+  } else {
+    res.redirect('/login');
+  }
 });
 
 app.get("/hello", (req, res) => {
@@ -40,36 +76,70 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase,
-  username: req.cookies["username"] };
+  user:users[req.cookies["user_id"]] };
   res.render("urls_index", templateVars);
 });
 
-app.get("/urls/new", (req,res) => {
-  const templateVars = {
-    username: req.cookies["username"] 
-  };
-    res.render("urls_new", templateVars);
-  
-})
+app.get("/urls/new", (req, res) => {
+  const templateVars = { urls: urlDatabase,
+  user:users[req.cookies["user_id"]] };
+  res.render("urls_new", templateVars);
+});
 
 app.get("/u/:shortURL", (req,res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 })
 
+app.get("/register", (req, res) => {
+   if (req.cookies.users_id) {
+   res.redirect('/urls');
+ } else {
+   res.render("urls_registration", { user: null });
+ }  
+});
+
+app.get("/login", (req, res) => {
+  if (req.cookies.users_id) {
+  res.redirect('/urls');
+} else {
+  res.render("urls_login", { user: null });
+}  
+});
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"]};
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], user: req.cookies["user_id"]};
   res.render("urls_show", templateVars);
 });
 
-app.post("/login", (req,res) => {
-  res.cookie("username", req.body.username);
-  res.redirect("/urls")
+
+
+app.post("/register", (req, res) => {
+
+   let newId = generateRandomString();
+  if (req.body.email === '' || req.body.password === '') {
+   res.send("<html><body>ERROR 400</body></html>\n");
+ } else {
+   for (const index of Object.keys(users)) {
+     if (req.body.email === users[index].email) {
+       res.send("<html><body>ERROR 400</body></html>\n");
+     }
+   }
+   users[newId] = {
+     id: newId,
+     email: req.body.email,
+     password: req.body.password
+   };
+   res.cookie("user_id", newId);
+   res.redirect("/urls");
+ } 
+
+ console.log(req.body);
 });
 
+
 app.post("/logout", (req,res) => {
-  res.clearCookie("username");
+  res.clearCookie("user_id");
   res.redirect("/urls");
 });
 
@@ -95,19 +165,24 @@ app.post("/urls/:id", (req, res) => {
 });  
 
 app.post('/login', (req, res) => {
-  res.status(201).cookie("username", req.body.username)
-  console.log(req.body.username);
-  //res.redirect('/urls')
+  console.log(req.body);
+  for (const index of Object.keys(users)) {
+    
+    if ((req.body.email === users[index].email) && (req.body.password === users[index].password)) {
+
+      res.status(200).cookie("user_id", users[index].id)
+      res.redirect('/urls')
+      
+    } else {
+      res.send("<html><body>ERROR 400</body></html>\n");
+    }
+  } 
+
+ 
+
 })
 
-function generateRandomString() {
-  let result = '';
-  characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  for (let i = 0; i < 6; i++) {
-    result += characters[Math.floor(Math.random() * chars.length)];
-  }
-  return result;
-}
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
