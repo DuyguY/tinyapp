7bf7e91f69d3a14eca311aa2e978e5ef46d55786
DuyguY <duygu.yldrm4820@gmail.com>
2021-12-15 13:47:30 -0500

    Add generateRandomString, urlsForUser

--- helpers.js
+++ helpers.js
@@ -1,14 +1,26 @@
-// to check email exists in database
-const getUserByEmail = (email, users) => {
-  for (const userId in users) {
-    const user = users[userId];
-    if(user.email === email) {
-      return user;
+//generate random string for shortURL
+function generateRandomString() {
+  let result = '';
+  characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  for (let i = 0; i < 6; i++) {
+    result += characters[Math.floor(Math.random() * characters.length)];
+  }
+  return result;
+};
+
+
+//returns a object of filtered URLs based on the userID that is currently logged in
+const urlsForUser = (id, urlDatabase) => {
+  const userURLs = {};
+  for (const shortURL in urlDatabase) {
+    if (urlDatabase[shortURL].userID === id) {
+      userURLs[shortURL] = urlDatabase[shortURL];
     }
   }
-  return null;
-}
+  return userURLs;
+};
 
 
 
-module.exports = { getUserByEmail }
+
+module.exports = { generateRandomString, urlsForUser}
